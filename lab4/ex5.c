e2ef2fe4b1758e2fd912a995805dd359fc23c48c
Lucas Thurler Gonçalves <163908689+LucasThurler@users.noreply.github.com>
2026-03-25 15:29:44 +0000

    consertei o exercicio 5 e comecei o lab5

--- lab4/ex5.c
+++ lab4/ex5.c
@@ -3,16 +3,14 @@
 void dump (void *p, int n) {
   unsigned char *p1 = p;
   while (n--) {
-    printf("%d ", *p1);
+    printf("%p - %02x\n", p1, *p1);
     p1++;
   }
 }
 
-int main() {
-    signed char sc = -1;
-    unsigned int ui = sc;
-
-    printf("valor de ui: %u -> na memória: ",ui);
-    dump(&ui, sizeof(ui));
-
+int main (void) {
+  signed char sc = -1;
+  unsigned int ui = sc;
+  dump(&ui, sizeof(ui));
+  return 0;
 }
